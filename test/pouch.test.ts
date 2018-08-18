3c10bd101416d6019479645e0e7a5ac3cb0fd7b7
dracornino <joel.delcompare@outlook.com>
2018-08-18 06:16:49 -0600

    PouchDB  7.0.0
    change jest testing to use in memory database 
    upgrade to pouch-db version 7.0.0

--- test/pouch.test.ts
+++ test/pouch.test.ts
@@ -1,91 +1,91 @@
 import "jest";
+import * as PouchDB from "pouchdb";
+import * as InMemoryPlugin from "pouchdb-adapter-memory";
 import { Container, Model, TypeName } from "../src/pouch";
-
+PouchDB.plugin(InMemoryPlugin);
 
 const unit = "Unit";
 const testing = "Testing";
 
 @TypeName(unit)
-class Unit extends Model<Unit>{
-    _id?: string;
-    name: string;
+class Unit extends Model<Unit> {
+  _id?: string;
+  name: string;
 }
 
 @TypeName(testing)
-class Testing extends Model<Testing>{
-    _id?: string;
-    name: string;
-    date: Date;
+class Testing extends Model<Testing> {
+  _id?: string;
+  name: string;
+  date: Date;
 }
 
 test("Testing for decorators to identify a class", async () => {
-    expect(Unit.__typename).toBe(unit);
-    expect(Testing.__typename).toBe(testing);
+  expect(Unit.__typename).toBe(unit);
+  expect(Testing.__typename).toBe(testing);
 });
 
-
 test("Test the pouchdb container developed by hehena", async () => {
-    let repository: Container = new Container("DB_TESTING", {
-        ajax: { cache: false },
-        auth: {
-            username: "ambit",
-            password: "ambit"
-        }
+  let repository: Container = new Container("DB_TESTING", {
+    ajax: { cache: false },
+    auth: {
+      username: "ambit",
+      password: "ambit"
+    },
+    adapter: "memory"
+  });
+  try {
+    await repository.addModels([Unit, Testing]);
+
+    let unitModel: Unit = { name: "Unit" };
+
+    let testingModel: Testing = { name: "Testing", date: new Date() };
+
+    let res = await Unit.insertOne(unitModel);
+    expect(res.ok).toBe(true);
+
+    Unit.change({ since: "now", live: true, include_docs: true }).on(
+      "change",
+      (info: any) => {
+        console.log(info);
+      }
+    );
+
+    res = await Unit.insertOne(unitModel);
+
+    expect(res.ok).toBe(true);
+
+    res = await Testing.insertOne(testingModel);
+
+    expect(res.ok).toBe(true);
+
+    let docs = await Unit.findAll({
+      selector: {
+        _id: { $gt: null }
+      }
     });
-    try {
-        await repository.addModels([Unit, Testing]);
 
-        let unitModel: Unit = { name: "Unit" };
+    expect(docs.length).toBe(2);
 
-        let testingModel: Testing = { name: "Testing", date: new Date() };
+    res = await Unit.deleteOne(docs[0]._id);
+    expect(res.ok).toBe(true);
 
-        let res = await Unit.insertOne(unitModel);
-        expect(res.ok).toBe(true);
+    res = await Unit.deleteOne(docs[1]._id);
+    expect(res.ok).toBe(true);
 
-        Unit.change({ since: "now", live: true, include_docs: true }).on("change",
-            (info: any) => {
-                console.log(info);
-            });
+    docs = await Testing.findAll({
+      selector: {
+        _id: { $gt: null }
+      }
+    });
 
-        res = await Unit.insertOne(unitModel);
+    expect(docs.length).toBe(1);
 
-        expect(res.ok).toBe(true);
-
-        res = await Testing.insertOne(testingModel);
-
-        expect(res.ok).toBe(true);
-
-        let docs = await Unit.findAll({
-            selector: {
-                _id: { "$gt": null }
-            }
-        });
-
-        expect(docs.length).toBe(2);
-
-
-        res = await Unit.deleteOne(docs[0]._id);
-        expect(res.ok).toBe(true);
-
-        res = await Unit.deleteOne(docs[1]._id);
-        expect(res.ok).toBe(true);
-
-
-        docs = await Testing.findAll({
-            selector: {
-                _id: { "$gt": null }
-            }
-        });
-
-        expect(docs.length).toBe(1);
-
-        res = await Testing.deleteOne(docs[0]._id);
-        expect(res.ok).toBe(true);
-
-
-    } catch (reason) {
-        fail(reason.message);
-    } finally {
-        await repository.close();
-    }
+    res = await Testing.deleteOne(docs[0]._id);
+    expect(res.ok).toBe(true);
+  } catch (reason) {
+    fail(reason.message);
+  } finally {
+    await repository.close();
+  }
 });
